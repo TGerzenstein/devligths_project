95c001aab12722384c7ccdda6457984a689a41b6
Tatiana <tatigerzens@gmail.com>
2024-09-05 20:10:12 -0300

    added route for users

--- API/routes/users.py
+++ API/routes/users.py
@@ -6,7 +6,9 @@ from typing import Annotated
 from models.users import User
 from config.database import COLLECTIONS
 from bson import ObjectId
-from models.schema import get_users
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 __all__: list[str] = ["users_router"]
@@ -16,30 +18,29 @@ users_router = APIRouter(prefix="/users",
                    responses={404: {"message": "Not found"}})
 
 
-""" 
-class CommonDep:
-    def __init__(self, start_date: str, end_date: str) -> None:
-        self.start_date = start_date
-        self.end_date = end_date
-        
-        
-@users_router.get("/s")
-async def get_users_parameters(commons: CommonDep = Depends()):
-    return f"Users {commons.start_date} and {commons.end_date}"
- """
 
-#find() retorna un cursor
-#model_dump: create dic
+def get_users(cursor):
+    return [User.from_mongo(user) for user in cursor]
 
-#GET Request Method
+# GET Request Method
 @users_router.get("/")
 async def list_users():
-    all_users =  get_users(COLLECTIONS["users"].find())
+    cursor = COLLECTIONS["users"].find()
+    all_users = get_users(cursor)
     return all_users
 
 
-#POST Request Method
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+
+# POST Request Method
 @users_router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: User):
-    COLLECTIONS["users"].insert_one(dict(user))
-    return {"result message": f"User created: {user}"} 
+
+    hashed_user = user.model_copy(update={"password": hash_password(user.password)})
+
+    COLLECTIONS["users"].insert_one(dict(hashed_user))
+    return {"result_message": f"User created: {user.name}"}
+
