c808e2d746849bcfea88e31946150bb8103435ab
Tatiana <tatigerzens@gmail.com>
2024-09-06 15:01:26 -0300

    updated database

--- API/config/database.py
+++ API/config/database.py
@@ -1,30 +1,30 @@
+__all__: list[str] = ["db", "product_collection"]
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from config.__base import *
 
 
-__all__: list[str] = ["db", "COLLECTIONS"]
-
-
 client = MongoClient(MONGODB_URI, server_api=ServerApi("1"))
-
 DB_NAME = "devlights_eCommerce_app"
-#create database
 db = client[DB_NAME]
 
 #collections in db
-COLLECTIONS = {
+""" COLLECTIONS = {
     "products": db["products"],
     "users": db["users"]
-}
+} """
+
+product_collection = db["products"]
+user_collection = db["users"]
 
 
 try:
-    client.admin.command("ping")
-    logger.info("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+    client.admin.command('ping')
+    print("<--- Pinged your deployment. You successfully connected to MongoDB! --->")
 
+except Exception as e:
+    print(f"Error conection",e)
 
 
 
