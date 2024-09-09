fb0f4598845e3d1899265dc65bea04e0ffed9c43
Tatiana <tatigerzens@gmail.com>
2024-09-09 16:20:23 -0300

    updated routes for products

--- API/models/products.py
+++ API/models/products.py
@@ -2,14 +2,36 @@ from pydantic import Field
 from pydantic import BaseModel
 from typing import Optional
 from pydantic_mongo import PydanticObjectId
-
+from enum import Enum
 
 __all__: list[str] = ["Product"] 
 
 
+
+
+class Category(str, Enum):
+    Nuevo = "Nuevo"
+    Usado = "Usado"
+    
+
 class Product(BaseModel):
-    id: PydanticObjectId = Field(default=None, alias="_id")   
+    id: Optional[str] = None
     name: str
     description: Optional[str] 
     price: float
-    quantity: int
+    quantity: int 
+    category: Optional[Category] = None  
+    
+    class Config:
+        from_attributes = True  
+
+    @classmethod
+    def product_db(cls, product: dict) -> "Product":
+        return cls(
+            id = str(product["_id"]),
+            name= product["name"],
+            description = product.get("description"), 
+            price = product["price"],
+            quantity = product["quantity"],
+            category = product.get("category", None) 
+        )
