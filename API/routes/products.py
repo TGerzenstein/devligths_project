ebd617d9c13d66d9fd570e54316856d30ef5753a
Tatiana <tatigerzens@gmail.com>
2024-09-11 18:10:11 -0300

    updated config for products

--- API/routes/products.py
+++ API/routes/products.py
@@ -1,10 +1,11 @@
+from bson import ObjectId
+
 from fastapi.routing import APIRouter
 from fastapi import HTTPException, status, Depends
 
 from models import *
-from models.products import Product
+from models.products import Product, Category
 from config.database import product_collection
-from bson import ObjectId
 
 
 __all__: list[str] = ["products_router"]
@@ -13,6 +14,7 @@ __all__: list[str] = ["products_router"]
 products_router = APIRouter(prefix="/products", 
                    tags=["products"],
                    responses={404: {"message": "Not found this"}})
+
 
 
 
@@ -30,6 +32,15 @@ async def list_products():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Products not found")
 
 
+@products_router.get("/category/{category}")
+async def filter_by_category(category: Category):
+    products = product_collection.find({"category": category.value})
+    filtered_products = [Product.product_db(product) for product in products]
+    if not filtered_products:
+        raise HTTPException(status_code=404, detail="No products found in this category")
+    return filtered_products
+
+
 @products_router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_product(product: Product):
     product_collection.insert_one(dict(product))
@@ -37,24 +48,27 @@ async def create_product(product: Product):
 
 
 
-""" 
 @products_router.put("/{id}")
 async def update_product(id: str, product: Product):
-    # Excluimos el campo `id` o `_id` del diccionario para evitar modificarlo
-    update_data = product.model_dump(exclude={"id", "_id"})
     
+    try:
+        object_id = ObjectId(id)
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid product ID")
+
+    update_data = product.model_dump(exclude={"id", "_id"})
     updated_product = product_collection.find_one_and_update(
-        {"_id": ObjectId(id)},  # Convertimos el id a ObjectId
-        {"$set": update_data},  # Solo actualizamos los campos válidos
-        return_document=True  # Devolvemos el documento actualizado
+        {"_id": object_id},  
+        {"$set": update_data}, 
+        return_document=True  
     )
     
     if updated_product:
+        updated_product['_id'] = str(updated_product['_id'])
         return {"message": "Product updated successfully", "product": updated_product}
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
-    
-     """
+
 
 
 @products_router.delete("/{id}")
