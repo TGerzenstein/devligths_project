172d386334e2fdc034a1757a06404a3013d48fdc
Tatiana <tatigerzens@gmail.com>
2024-09-12 14:56:37 -0300

    incluided routes for projects

--- API/routes/__init__.py
+++ API/routes/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from routes.products import products_router
 from routes.users import users_router
+from routes.auth_users import auth_users
+from routes.test import app_test
 
 
 #variable
@@ -10,3 +12,5 @@ __all__: list[str] = ["api_router"]
 api_router = APIRouter(prefix="/api")
 api_router.include_router(products_router)
 api_router.include_router(users_router)
+api_router.include_router(auth_users)
+api_router.include_router(app_test)
