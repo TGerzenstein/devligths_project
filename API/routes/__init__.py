9286b9e55339e18d883a32aaf48f2fc042c8b2c5
Tatiana <tatigerzens@gmail.com>
2024-09-17 19:34:41 -0300

    updated config routes

--- API/routes/__init__.py
+++ API/routes/__init__.py
@@ -1,16 +1,14 @@
+__all__: list[str] = ["api_router"]
+
 from fastapi import APIRouter
 from routes.products import products_router
 from routes.users import users_router
-from routes.auth_users import auth_users
+from routes.auth_users import auth_user
 from routes.test import app_test
 
-
-#variable
-#solo api_router será exportado
-__all__: list[str] = ["api_router"]
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(products_router)
 api_router.include_router(users_router)
-api_router.include_router(auth_users)
+api_router.include_router(auth_user)
 api_router.include_router(app_test)
